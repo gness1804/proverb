4c3504a9ff6c903810acdb0459ccfbdec279ca12
gness1804 <graham.nessler@gmail.com>
2022-05-31 19:31:06 -0500

    all tests passing

--- proverb.ts
+++ proverb.ts
@@ -1,3 +1,11 @@
-export function proverb() {
-  throw new Error('Remove this statement and implement this function')
+export function proverb(...args: string[]): string {
+  const res = args.reduce((acc: string, curr: string, index: number) => {
+    if (index === args.length - 1) {
+      acc += `And all for the want of a ${args[0]}.`;
+    } else {
+      acc += `For want of a ${args[index]} the ${args[index + 1]} was lost.\n`
+    }
+    return acc;
+  }, '');
+  return res;
 }
